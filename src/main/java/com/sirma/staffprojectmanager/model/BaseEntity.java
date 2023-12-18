a18ff004fce62f017ae1237a18ad164eb9878bc7
dobrin_r <rashkov09@gmail.com>
2023-12-18 12:01:59 +0200

    base entity class

--- src/main/java/com/sirma/staffprojectmanager/model/BaseEntity.java
+++ src/main/java/com/sirma/staffprojectmanager/model/BaseEntity.java
@@ -1,2 +1,21 @@
-package com.sirma.staffprojectmanager.model;public class BaseEntity {
+package com.sirma.staffprojectmanager.model;
+
+public class BaseEntity {
+
+	private Long id;
+
+	public BaseEntity(Long id) {
+		this.id = id;
+	}
+
+	public BaseEntity() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
