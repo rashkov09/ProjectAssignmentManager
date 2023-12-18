fc293b708aa2c0c631fc2e482979749c725d8028
dobrin_r <rashkov09@gmail.com>
2023-12-18 14:31:27 +0200

    added repository for project assignment

---
+++ src/main/java/com/sirma/staffprojectmanager/repository/ProjectAssignmentRepository.java
@@ -0,0 +1,8 @@
+package com.sirma.staffprojectmanager.repository;
+
+import com.sirma.staffprojectmanager.model.ProjectAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectAssignmentRepository extends JpaRepository<ProjectAssignment, Long> {
+
+}
