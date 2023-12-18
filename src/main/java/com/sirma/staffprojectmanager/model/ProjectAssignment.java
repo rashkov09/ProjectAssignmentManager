b9092d7839726238b1af59d781923b62e288ebc7
dobrin_r <rashkov09@gmail.com>
2023-12-18 12:03:18 +0200

    added project assigment class

---
+++ src/main/java/com/sirma/staffprojectmanager/model/ProjectAssignment.java
@@ -0,0 +1,49 @@
+package com.sirma.staffprojectmanager.model;
+
+import java.time.LocalDate;
+
+public class ProjectAssignment extends BaseEntity{
+	private Long employeeId;
+	private Long projectId;
+	private LocalDate dateFrom;
+	private LocalDate dateTo;
+
+	public ProjectAssignment(Long employeeId, Long projectId, LocalDate dateFrom, LocalDate dateTo) {
+		this.employeeId = employeeId;
+		this.projectId = projectId;
+		this.dateFrom = dateFrom;
+		this.dateTo = dateTo;
+	}
+
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public Long getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(Long projectId) {
+		this.projectId = projectId;
+	}
+
+	public LocalDate getDateFrom() {
+		return dateFrom;
+	}
+
+	public void setDateFrom(LocalDate dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+
+	public LocalDate getDateTo() {
+		return dateTo;
+	}
+
+	public void setDateTo(LocalDate dateTo) {
+		this.dateTo = dateTo;
+	}
+}
