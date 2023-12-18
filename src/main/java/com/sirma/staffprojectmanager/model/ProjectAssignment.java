62d823433169f3d177625e3c282edf859d44e5ce
dobrin_r <rashkov09@gmail.com>
2023-12-18 19:24:37 +0200

    added native named query and mapping

--- src/main/java/com/sirma/staffprojectmanager/model/ProjectAssignment.java
+++ src/main/java/com/sirma/staffprojectmanager/model/ProjectAssignment.java
@@ -1,13 +1,43 @@
 package com.sirma.staffprojectmanager.model;
 
-import jakarta.persistence.Column;
+import com.sirma.staffprojectmanager.model.dto.ResultDto;
+import jakarta.persistence.ColumnResult;
+import jakarta.persistence.ConstructorResult;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.NamedNativeQuery;
+import jakarta.persistence.SqlResultSetMapping;
 
 import java.time.LocalDate;
-
+@SqlResultSetMapping(
+	name = "ResultDtoMapping",
+	classes = {
+		@ConstructorResult(
+			targetClass = ResultDto.class,
+			columns = {
+				@ColumnResult(name = "emp1", type = Long.class),
+				@ColumnResult(name = "emp2", type = Long.class),
+				@ColumnResult(name = "project_id", type = Long.class),
+				@ColumnResult(name = "overlap_days", type = Integer.class)
+			}
+		)
+	}
+)
+@NamedNativeQuery(
+	name = "findOverlappingData",
+	query = "SELECT " +
+	        "a.employee_id AS emp1, " +
+	        "b.employee_id AS emp2, " +
+	        "a.project_id AS project_id, " +
+	        "(LEAST(COALESCE(a.date_to, CURRENT_DATE), COALESCE(b.date_to, CURRENT_DATE)) " +
+	        " - GREATEST(a.date_from, b.date_from)) AS overlap_days " +
+	        "FROM project_assignment a " +
+	        "INNER JOIN project_assignment b ON a.employee_id < b.employee_id " +
+	        "    AND a.project_id = b.project_id " +
+	        "WHERE " +
+	        "(a.date_from, COALESCE(a.date_to, CURRENT_DATE)) OVERLAPS (b.date_from, COALESCE(b.date_to, CURRENT_DATE)) " +
+	        "AND NOT (a.employee_id = b.employee_id)",
+	resultSetMapping = "ResultDtoMapping"
+)
 @Entity
 public class ProjectAssignment extends BaseEntity {
 
