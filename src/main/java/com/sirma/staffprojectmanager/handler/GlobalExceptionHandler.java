3632e31b4a8d405925348025eac59f719d36d9c5
dobrin_r <rashkov09@gmail.com>
2023-12-21 18:37:20 +0200

    added handle for reprocessing exception

--- src/main/java/com/sirma/staffprojectmanager/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sirma/staffprojectmanager/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.sirma.staffprojectmanager.handler;
 
+import com.sirma.staffprojectmanager.exception.InvalidFileDataException;
 import com.sirma.staffprojectmanager.exception.ProjectAssignmentNotFoundException;
+import org.springframework.boot.context.properties.bind.validation.BindValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,5 +14,9 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(ProjectAssignmentNotFoundException.class)
 	public ResponseEntity<String> handleEmployeeNotFountException(ProjectAssignmentNotFoundException ex) {
 		return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+	}
+	@ExceptionHandler(InvalidFileDataException.class)
+	public ResponseEntity<String> handleEmployeeNotFountException(InvalidFileDataException ex) {
+		return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 }
